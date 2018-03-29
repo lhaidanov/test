95f617b111ba285f3c17c65e81a4387a31dd4701
Leonid <leonid.haidanov@gmail.com>
2018-03-29 22:53:31 +0300

    was added some new unit tests

--- src/test/java/TestMavenClass.java
+++ src/test/java/TestMavenClass.java
@@ -14,20 +14,16 @@ public class TestMavenClass {
         String regex = "(?:\\+38)? ?(?:0\\d{2}|\\(0\\d{2}\\)) ?\\d{2}(?:\\d{3}|\\d[- ]\\d{2}[- ]|[ -]\\d{2}[ -]\\d)\\d{2}";
         return text.matches(regex);
     }
-
+    
+//correct data with "-" and without "+38"
     @Test
     public static void test1_isPhone() {
-        String testData = "097515-69-00";
+        String testData = "097 515-69-00";
         boolean result = isPhone(testData);
         Assert.assertTrue(result, "Test1");
     }
-//    @Test
-//    public static void test6_isPhone() {
-//        String testData = "097515-69-00";
-//        boolean result = isPhone(testData);
-//        Assert.assertTrue(result, "Test6");
-//    }
 
+//correct data without "-" and with "+38"
     @Test
     public static void test2_isPhone() {
         String testData = "+380975156900";
@@ -35,32 +31,60 @@ public class TestMavenClass {
         Assert.assertTrue(result, "Test2");
     }
 
+//empty value
+    @Test
+    public static void test8_isPhone() {
+        String testData = "";
+        boolean result = isPhone(testData);
+        Assert.assertFalse(result, "Test8");
+    }
+
+// incorrect data
     @Test
     public static void test3_isPhone() {
         String testData = "asdaddasds";
         boolean result = isPhone(testData);
-        Assert.assertFalse(result);
+        Assert.assertFalse(result, "Test3");
     }
 
+//too long numb
     @Test
     public static void test4_isPhone() {
-        String testData = "+380975156900 65454";
+        String testData = "+3809751569009";
         boolean result = isPhone(testData);
-        Assert.assertFalse(result, "Test4 with too long numb");
+        Assert.assertFalse(result, "Test4");
     }
 
+//too short numb
+    @Test
+    public static void test9_isPhone() {
+        String testData = "+3809751569009";
+        boolean result = isPhone(testData);
+        Assert.assertFalse(result, "Test9");
+    }
+
+// tab instead of space
     @Test
     public static void test5_isPhone() {
-        String testData = "+38 0975 15 6900";
+        String testData = "+38097   5156900";
         boolean result = isPhone(testData);
         Assert.assertFalse(result, "Test5");
     }
 
+// "-" on unexpected place
     @Test
     public static void test7_isPhone() {
         String testData = "+38097515--6900";
         boolean result = isPhone(testData);
         Assert.assertFalse(result, "Test7");
+    }
+
+//space on unexpected place
+    @Test
+    public static void test10_isPhone() {
+        String testData = "+38097515--6900";
+        boolean result = isPhone(testData);
+        Assert.assertFalse(result, "Test10");
     }
 
 }
